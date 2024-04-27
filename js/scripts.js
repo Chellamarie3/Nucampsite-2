541c108c3c1d3cc0111159ca0ee0d15eb3836d7d
Chelsea <cjohnson594@outlook.com>
2024-04-26 20:45:16 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,33 @@ carouselButton.addEventListener("click", function () {
     carousel.cycle();
   }
 });
+async function fetchWeather() {
+  const apiKey = process.env.OPEN_WEATHER_API_KEY;
+  const city = "beaverton";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  const response = await fetch(url).catch(() => {
+    console.log("error calling url");
+  });
+  const data = await response.json().catch(() => {
+    console.log("error parsing response");
+  });
+  displayWeather(data);
+}
+async function displayWeather(data) {
+  const temp = data.main.temp;
+  const description = data.weather[0].description;
+  const icon = data.weather[0].icon;
+  const windSpeed = data.wind.speed;
+  const name = data.name;
+
+  console.log(`temp ${temp} description ${description} icon ${icon}`);
+  const img = document.createElement("img");
+  img.src = `https://openweathermap.org/img/w/${icon}.png`;
+  document.getElementById("weather-icon").appendChild(img);
+  document.getElementById("weather-temp").textContent = `${temp} \u00B0`;
+  document.getElementById("weather-description").textContent = description;
+  document.getElementById("wind-speed").textContent = windSpeed;
+  document.getElementById("name").textContent = ` - ${name}`;
+}
+
+fetchWeather();
